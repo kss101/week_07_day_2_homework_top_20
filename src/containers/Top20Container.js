e3a9507ec730050611eb28f9d834b441ecae9f42
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-12-15 17:42:26 +0000

    Set up request to Apple to get UK top 20 songs...

--- src/containers/Top20Container.js
+++ src/containers/Top20Container.js
@@ -2,8 +2,27 @@ import {useEffect, useState} from "react";
 import Top20List from "../components/Top20List";
 
 const Top20Comtainer = () => {
+
+    const [top20UKSongs, setTop20UKSongs] = useState(null);
+
+        const getTop20 = () => {
+            console.log("Getting Top 20 Listings");
+            fetch(`https://itunes.apple.com/gb/rss/topsongs/limit=20/json`)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                setTop20UKSongs(data)
+            })
+        };
+
+        useEffect(() => {
+            getTop20();
+          },[]);
+
     return (
         <>
+            <h1>UK Top 20 Listings...</h1>
             <Top20List />
         </>
     )
